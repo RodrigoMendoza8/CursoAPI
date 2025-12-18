0de86813f1f12db5324c8d6afcfa3087a4493e48
RodrigoMendoza8 <rodrigomendozardgz824@gmail.com>
2025-12-18 15:50:22 -0600

    Variable dinamica

--- curso Youtube/main.py
+++ curso Youtube/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# Variable dinamica en la ruta
+# Fasti API depende del tipo de parametro, lo ideal es epecificar el tipo de parametro en cada variable
+@app.get("/items/{item_id}")
+def read_item(item_id: int,):
+    return {'item_id': item_id}
