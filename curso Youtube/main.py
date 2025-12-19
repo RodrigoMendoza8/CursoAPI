7237368f3d852b8ad9ef3c8cefb963e1da05e7be
RodrigoMendoza8 <rodrigomendozardgz824@gmail.com>
2025-12-18 21:35:05 -0600

    uso de pydantic

--- curso Youtube/main.py
+++ curso Youtube/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+products = [
+    Product(id=1, name="Laptop", description="A high performance laptop", price=1200.00, quantity=10),
+    Product(id=2, name="Smartphone", description="A latest model smartphone", price=800.00, quantity=25),
+    Product(id=3, name="Headphones", description="Noise cancelling headphones", price=150.00, quantity=50)
+]
 
+@app.get("/products")
+def get_products():
+    return products
 # Variable dinamica en la ruta
 # Fasti API depende del tipo de parametro, lo ideal es epecificar el tipo de parametro en cada variable
 @app.get("/items/{item_id}")
